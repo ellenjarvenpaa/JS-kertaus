e5a56f4420f23b5d1880c683fb31ebe793b7f478
Ellen Järvenpää <ellen.jarvenpaa@metropolia.fi>
2023-08-23 18:52:28 +0300

    5 valmis

--- teht_5/t5.js
+++ teht_5/t5.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const number = +prompt('Enter a positive number');
+let sum = 0;
 
-for (let i = number; i != 0; i += i) {
-  console.log(i);
+for (let i = number; i > 0; i -= 1) {
+  sum += i;
 }
+
+document.getElementById('kohde').innerHTML = sum;
