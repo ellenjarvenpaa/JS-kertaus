4706a9309b6b11bf6ca8f77aa18121c9c0e50d82
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2023-08-30 09:04:16 +0300

    js kertaus 3

--- JS-KERTAUS-2/teht_3/t3.js
+++ JS-KERTAUS-2/teht_3/t3.js
@@ -1,8 +1,18 @@
 'use strict';
 
 const numbers = [];
+let jatka = true;
 
-const input = prompt('Enter a number or "done" to finish');
-while (input != 'done') {
+while (jatka) {
+  const input = prompt('Enter a number or "done" to finish');
+  if (input === 'done') {
+    jatka = false;
+  }
   numbers.push(input);
+}
+
+for (const number of numbers) {
+  if (number % 2 === 0) {
+    document.getElementById('kohde').innerHTML = number.join(', ');
+  }
 }
